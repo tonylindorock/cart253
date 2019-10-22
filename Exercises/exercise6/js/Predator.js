7d3e27fc7799ac6aa404484e90bbc9663f652c6a
Tony LindoRock <yichen@yichens-mbp.wireless.concordia.ca>
2019-10-22 16:44:13 -0400

    Ex6: Fixed Predator dist() parameters
    thisX and thisY do not exist and are changed to this.x and this.y

--- Exercises/exercise6/js/Predator.js
+++ Exercises/exercise6/js/Predator.js
@@ -5,7 +5,7 @@
 // the screen and consume Prey objects to maintain its health.
 
 /////////////////////
-// ~9 ERRORS IN HERE
+// ~8 ERRORS IN HERE
 /////////////////////
 
 class Predator {
@@ -108,7 +108,7 @@ class Predator {
   // the predator's. If the prey dies, it gets reset.
   handleEating(prey) {
     // Calculate distance from this predator to the prey
-    let d = dist(thisX, thisY, prey.x, prey.y);
+    let d = dist(this.x, this.y, prey.x, prey.y); //////////////// FIXED
     // Check if the distance is less than their two radii (an overlap)
     if (d < this.radius + prey.radius) {
       // Increase predator health and constrain it to its possible range
