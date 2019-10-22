c98b2543e26522a865e52a749856432e39eec7a2
Tony LindoRock <yichen@yichens-mbp.wireless.concordia.ca>
2019-10-22 17:03:24 -0400

    Ex6; Fixed tiger.handleInput()
    tiger.handleInput() does not present and  is added

--- Exercises/exercise6/js/script.js
+++ Exercises/exercise6/js/script.js
@@ -8,7 +8,7 @@
 // The predator loses health over time, so must keep eating to survive.
 
 ////////////////////
-// 1 ERRORS IN HERE
+// 0 ERRORS IN HERE
 ////////////////////
 
 // Our predator
@@ -39,6 +39,7 @@ function draw() {
   background(0); //////////////// FIXED
 
   // Handle input for the tiger
+  tiger.handleInput(); //////////////// FIXED
 
   // Move all the "animals"
   tiger.move();
