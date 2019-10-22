75276121c0759117de2375d46bed24317a84d268
Tony LindoRock <yichen@yichens-mbp.wireless.concordia.ca>
2019-10-22 16:42:16 -0400

    Ex6: Fixed Predator constructor parameter name
    feltColor is incorrect spelling and is changed to fillColor

--- Exercises/exercise6/js/Predator.js
+++ Exercises/exercise6/js/Predator.js
@@ -5,7 +5,7 @@
 // the screen and consume Prey objects to maintain its health.
 
 /////////////////////
-// ~10 ERRORS IN HERE
+// ~9 ERRORS IN HERE
 /////////////////////
 
 class Predator {
@@ -14,7 +14,7 @@ class Predator {
   //
   // Sets the initial values for the Predator's properties
   // Either sets default values or uses the arguments provided
-  construction(x, y, speed, feltColor, radius) {
+  construction(x, y, speed, fillColor, radius) { //////////////// FIXED
     // Position
     this.x = x;
     this.y = y;
