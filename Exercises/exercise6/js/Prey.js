65b676bc83648e8b9c9b6ae44b71d3e6f43fa861
Tony LindoRock <yichen@yichens-mbp.wireless.concordia.ca>
2019-10-22 17:13:06 -0400

    Ex6; Fixed Prey tx and ty declaration
    random(0,0) is not going to work and is changed to random(0,100)

--- Exercises/exercise6/js/Prey.js
+++ Exercises/exercise6/js/Prey.js
@@ -5,7 +5,7 @@
 // the screen and be consumed by Predator objects.
 
 /////////////////////////
-// ~2 ERRORS IN HERE
+// ~1 ERRORS IN HERE
 /////////////////////////
 
 class Prey { //////////////// FIXED
@@ -23,8 +23,9 @@ class Prey { //////////////// FIXED
     this.vy = 0;
     this.sped = speed;
     // Time properties for noise() function
-    this.tx = random(0, 0); // To make x and y noise different
-    this.ty = random(0, 0); // we use random starting values
+    //////////////// FIXED
+    this.tx = random(0, 100); // To make x and y noise different
+    this.ty = random(0, 100); // we use random starting values
     // Health properties
     this.maxHealth = radius;
     this.health = this.maxHealth; // Must be AFTER defining this.maxHealth
@@ -76,26 +77,26 @@ class Prey { //////////////// FIXED
     //
     // Draw the prey as an ellipse on the canvas
     // with a radius the same size as its current health.
-    display() {
-      push();
-      noStroke();
-      fill(this.fillColor);
-      this.radius = this.health;
-      ellipse(this.x, this.y, this.radius * 2); //////////////// FIXED
-      pop();
-    }
+  display() {
+    push();
+    noStroke();
+    fill(this.fillColor);
+    this.radius = this.health;
+    ellipse(this.x, this.y, this.radius * 2); //////////////// FIXED
+    pop();
+  }
 
     // reset
     //
     // Set the position to a random location and reset health
     // and radius back to default
-    reset() {
-      // Random position
-      this.x = random(0, width);
-      this.y = random(0, height);
-      // Default health
-      this.health = this.maxHealth;
-      // Default radius
-      this.radius = this.health;
+  reset() {
+    // Random position
+    this.x = random(0, width);
+    this.y = random(0, height);
+    // Default health
+    this.health = this.maxHealth;
+    // Default radius
+    this.radius = this.health;
     }
-  }
+}
