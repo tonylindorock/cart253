ed085746f4dc8ebd0a82d0a777723b2049e3836e
Tony LindoRock <yichen@Yichens-MacBook-Pro.local>
2019-11-11 23:08:15 -0500

    P3: Added main menu

--- Project 3/js/script.js
+++ Project 3/js/script.js
@@ -1,12 +1,19 @@
 /*****************
+- Final Project -
+Simple Defence
+By Yichen Wang
 
-Title of Project
-Yichen Wang
+Simple Defence is a simple game with simple UI and straightforward rules.
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
 
 ******************/
+
+let starting = false;
+let playing = false;
+let gameOver = false;
+let singlePlayer = true;
+
+const SELECTED = "#4ddeff";
 
 function preload() {
 
@@ -14,8 +21,70 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  background(100);
+
+  textFont("Verdana");
+  textStyle(BOLD);
+}
+
+function draw() {
+  background(100);
+  if (!starting){
+    displayMainMenu();
+  }else{
+    if(!playing){
+      displaySecondMenu()
+    }else{
+
+    }
+  }
 
 }
-function draw() {
 
+function displayMainMenu(){
+  push();
+  fill(255);
+  textAlign(CENTER,CENTER);
+  rectMode(CENTER);
+  textSize(64);
+  text("S I M P L E   D E F E N C E",width/2,height/2);
+  pop();
+
+  checkMainMenuButtons();
+}
+
+function checkMainMenuButtons(){
+  push();
+  textAlign(CENTER,CENTER);
+  textSize(32);
+  if (mouseY>height-height/3){
+    fill(SELECTED);
+    text("P L A Y",width/2,height/2+200);
+    if (mouseIsPressed){
+      starting = true;
+    }
+  }else{
+    fill(255);
+    text("P L A Y",width/2,height/2+200);
+  }
+  pop();
+}
+
+function displaySecondMenu(){
+  push();
+  fill(255);
+  textAlign(CENTER,CENTER);
+  rectMode(CENTER);
+  textSize(32);
+  text("C H O O S E   Y O U R   M A P",width/2,75);
+  pop();
+
+  checkSecondMenuButton();
+}
+
+function checkSecondMenuButton(){
+  push();
+  textAlign(CENTER,CENTER);
+  textSize(32);
+  pop();
 }
